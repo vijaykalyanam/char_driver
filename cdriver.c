3a07c9dda84cdd3c9eb386d8e587dc719079be49
vkalyanam <vkalyanam@lantronix.com>
2016-12-26 17:46:53 +0530

    In the latest kernel, we need to use create_device API for char device.

--- cdriver.c
+++ cdriver.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>         // Core header for loading LKMs into the kernel
 #include <linux/kernel.h>         // Contains types, macros, functions(printk) for the kernel
 #include <linux/moduleparam.h>    // Kernel module param
+#include <linux/version.h>
 
 #include <linux/cdev.h>
 #include <linux/fs.h>
@@ -19,7 +20,7 @@ MODULE_DESCRIPTION("This is just a Char driver");
 
 static char *name = "cvijay";
 module_param(name, charp, S_IRUSR);
-unsigned int major=117, minor=1;
+unsigned int major, minor=1, count=1;
 
 struct cdev *cdev = NULL;
 static const struct file_operations cdriver_fops = {
@@ -32,13 +33,23 @@ static const struct file_operations cdriver_fops = {
 
 static int __init cdriver_init(void) {
 
-	int rc;
+	int rc=0;
 	dev_t dev = 0;
 	rc = cdriver_alloc(&cdev);
 	if (!rc) {
-		dev = MKDEV(major, minor);
-		printk(KERN_INFO "chardriver :dev id :%d\n", dev);
-
+#if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,0)
+		rc = alloc_chrdev_region(&dev, minor, count, name); 
+		if (rc) {
+			printk(KERN_ERR "%s char device allocation failed\n", name);
+			goto del_cdev;
+		}
+		rc = register_chrdev_region(dev, count, name);
+		if (rc) {
+			printk(KERN_ERR "%s char device registeration failed\n", name);
+			goto del_cdev;
+		}
+#else
+#endif
 		cdev->owner = THIS_MODULE;
 		cdev->dev = dev;
 		cdev_init(cdev, &cdriver_fops);
@@ -46,9 +57,11 @@ static int __init cdriver_init(void) {
 
 		rc = cdev_add(cdev, dev, minor);
 		if (rc == 0) { 
-			printk(KERN_INFO "char device regiestered with kernel :%d\n", dev);
+			printk(KERN_INFO " %s char device is up. Major no :%u minor :%u\n",
+					name, MAJOR(dev), MINOR(dev));
 			return 0;
 		}
+del_cdev:
 		printk(KERN_ERR "cdev registration with kernel failed rc :0x%x\n",rc);
 		cdev_del(cdev);
 	}
@@ -57,8 +70,10 @@ static int __init cdriver_init(void) {
 
 static void __exit cdriver_exit(void) {
 
-	if (cdev) cdev_del(cdev);
-	printk(KERN_INFO "cdev with dev id :%d unloaded\n", cdev->dev);
+	if (cdev) {
+		cdev_del(cdev);  //Unregister from kernel first to aviod kernel operations.
+		unregister_chrdev_region(cdev->dev, count);
+	}
 	return;
 }
 
